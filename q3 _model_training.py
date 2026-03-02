82189003c108117500e7256dade3b30a13b98530
nalin7sharma <2005nalinsharma@gmail.com>
2026-03-02 19:19:39 +0530

    Model Training & Supervised Evaluation

--- q3 _model_training.py
+++ q3 _model_training.py
@@ -11,26 +11,21 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-# ----------------------------
+
 # CONFIG
-# ----------------------------
 IMAGE_FOLDER = "Delhi-Airshed-LandUse-AI-Audit\\rgb"
 BATCH_SIZE = 32
 EPOCHS = 5
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# ----------------------------
 # Load Train/Test CSV
-# ----------------------------
 train_df = pd.read_csv("train.csv")
 test_df = pd.read_csv("test.csv")
 
 print("Train samples:", len(train_df))
 print("Test samples:", len(test_df))
 
-# ----------------------------
 # Encode labels
-# ----------------------------
 classes = train_df["label"].unique()
 class_to_idx = {label: idx for idx, label in enumerate(classes)}
 idx_to_class = {v: k for k, v in class_to_idx.items()}
@@ -38,9 +33,8 @@ idx_to_class = {v: k for k, v in class_to_idx.items()}
 train_df["label_idx"] = train_df["label"].map(class_to_idx)
 test_df["label_idx"] = test_df["label"].map(class_to_idx)
 
-# ----------------------------
+
 # Custom Dataset
-# ----------------------------
 class LandUseDataset(Dataset):
     def __init__(self, dataframe, transform=None):
         self.df = dataframe
@@ -61,9 +55,8 @@ class LandUseDataset(Dataset):
 
         return image, label
 
-# ----------------------------
+
 # Image Transform
-# ----------------------------
 transform = transforms.Compose([
     transforms.Resize((128, 128)),
     transforms.ToTensor(),
@@ -75,9 +68,8 @@ test_dataset = LandUseDataset(test_df, transform)
 train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
-# ----------------------------
 # Load ResNet18
-# ----------------------------
+
 model = models.resnet18(pretrained=True)
 
 # Modify final layer
@@ -86,15 +78,11 @@ model.fc = nn.Linear(model.fc.in_features, num_classes)
 
 model = model.to(DEVICE)
 
-# ----------------------------
 # Loss & Optimizer
-# ----------------------------
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-# ----------------------------
 # Training Loop
-# ----------------------------
 for epoch in range(EPOCHS):
     model.train()
     total_loss = 0
@@ -115,9 +103,7 @@ for epoch in range(EPOCHS):
 
 print("Training Complete!")
 
-# ----------------------------
 # Evaluation
-# ----------------------------
 model.eval()
 all_preds = []
 all_labels = []
@@ -140,9 +126,7 @@ f1 = f1_score(all_labels, all_preds, average="weighted")
 print("Accuracy:", acc)
 print("F1 Score:", f1)
 
-# ----------------------------
 # Confusion Matrix
-# ----------------------------
 cm = confusion_matrix(all_labels, all_preds)
 
 plt.figure(figsize=(8,6))
